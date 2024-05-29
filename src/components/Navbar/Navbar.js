cd68df26f5ef823c6fd3ae3640e14776377e7895
Bhavesh Bhagtani <99315484+bhagtanib@users.noreply.github.com>
2024-05-29 04:08:58 -0700

    Changed project details

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
 // Navbar.js
 import React from "react";
 import classes from "./Navbar.module.css"; // Import your CSS file for styling
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleAuthClick = () => {
+    navigate("/auth")
+  }
   return (
     <div className={classes.navbar}>
       <div className={classes.navbarLeft}>
@@ -10,11 +15,9 @@ const Navbar = () => {
           src="https://png.pngtree.com/png-vector/20220702/ourmid/pngtree-coding-logo-template-illustration-design-png-image_5673078.png"
           alt="laptop-coding"
         />
-        <span>Practice </span>
       </div>
       <div className={classes.navbarRight}>
-        <button> Login </button>
-        <button> Join </button>
+        <button onClick={handleAuthClick}> Login </button>
       </div>
     </div>
   );
